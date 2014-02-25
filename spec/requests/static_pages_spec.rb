a079d17ad467ab7c7b07a366fb0cd557fa67dbe3
kuroicon <fzhan@wisc.edu>
2014-02-24 19:30:36 -0500

    hw5 fin

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,21 +15,35 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
+    it { should have_selector('h1', text: 'About') }
     it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    
   end
 end
