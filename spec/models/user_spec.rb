5c371cc6eeb5b87cf34d21cfb0fb0c3a02a5e5dd
kurokocon <kuroicon@linux-1vs7.site>
2014-03-05 01:02:39 -0500

    hw6 exercise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,7 +45,7 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn foo@bar..com]
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
@@ -96,4 +96,16 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
+  
 end
