6f5e6406bc90f226c832d3307d328b2fe0ab4c57
kurokocon <kuroicon@linux-1vs7.site>
2014-04-08 00:18:01 -0400

    After ch9 9.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,11 +17,39 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+   
+  end
   
+  def index
+    @users = User.all
+  end
+  
+  def update
+    
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
