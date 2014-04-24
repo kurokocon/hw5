98041fe4da69b578c9868952cde6af387c7be5c2
kurokocon <kuroicon@linux-1vs7.site>
2014-04-24 18:03:45 -0400

    finished tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
+  
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
   private
 
     def create_remember_token
